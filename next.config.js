44646bf5d9cfc0edf2e3224c464841a7030c1875
Harsh Ghodkar <harshghodkar@gmail.com>
2022-02-03 15:46:36 +0530

    Delete next.config.js

--- next.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  images: {
-    loader: "akamai",
-    path: "/",
-    // domains: ["user-images.githubusercontent.com"],
-  },
-  basePath: "/gssoc-website",
-  assetPrefix: "/gssoc-website",
-};
